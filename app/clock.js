facab8ee51f9575ec75a39b716e60675b5159cce
tfriedman <sfriedman@learnvest.com>
2017-03-09 10:14:11 -0500

    add analog clock

--- app/clock.js
+++ app/clock.js
@@ -1,32 +1,37 @@
 const React = require('react');
 
+const {ApAnalogClock, ApAnalogClockStyle} =  require('apeman-react-clock');
+
 class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {date: new Date()};
+    // }
 
-    componentDidMount() {
-        this.timerID = setInterval( 
-            () => this.tick(), 
-            1000
-        );
-    }
+    // componentDidMount() {
+    //     this.timerID = setInterval( 
+    //         () => this.tick(), 
+    //         1000
+    //     );
+    // }
 
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
+    // componentWillUnmount() {
+    //     clearInterval(this.timerID);
+    // }
 
-    tick() {
-        this.setState({
-            date: new Date()
-        });
-    }
+    // tick() {
+    //     this.setState({
+    //         date: new Date()
+    //     });
+    // }
 
     render() {
         return (
             <div className="clock">
-                <h2 className="inset">{this.state.date.toLocaleTimeString()}</h2>
+                {/*<h2 className="inset">{this.state.date.toLocaleTimeString()}</h2>*/}
+                
+                <ApAnalogClockStyle scoped backgroundColor="#daba52"/>
+                <ApAnalogClock />
             </div>
         );
     }
